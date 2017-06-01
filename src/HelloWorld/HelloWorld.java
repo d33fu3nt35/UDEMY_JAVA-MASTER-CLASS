17d14b4486fc0a60a1b0e60ff7854eb0217728bb
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-05-31 20:04:34 -0500

    HelloWorld App Commit

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+/**
+ * Created by daniel on 5/31/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Daniel!");
+    }
+}
