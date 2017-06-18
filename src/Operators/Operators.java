a3a9e59310a8d4230fc1ad95dad26fb5de59f35b
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-06-17 22:43:11 -0500

    Wen't over JAVA operators & a little bit of if/else if statements.

--- src/Operators/Operators.java
+++ src/Operators/Operators.java
@@ -49,15 +49,48 @@ public class Operators {
         else if (isAlien == true)
             System.out.println("Its an alien!!");
 
-        int topScore = 100;
+        int topScore = 80;
         if(topScore == 100)
             System.out.println("High Score!!!");
         else if (topScore != 100)
             System.out.println("Sorry, you lose!");
 
-        int secondTopscore = 60;
-        if(topScore > secondTopscore && topScore < 100)
+        int secondTopscore = 95;
+        if((topScore > secondTopscore) && (topScore < 100))
             System.out.println("Greater than second top score and less then 100!");
+
+        if((topScore > 90)  || (secondTopscore <= 90))
+            System.out.println("One of these tests is true!");
+
+        int newValue = 50;
+        if(newValue == 50)
+            System.out.println("This is true!");
+
+        boolean isCar = false;
+        if(isCar == true)
+            System.out.println("This is not suppose to happen!");
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if(wasCar)
+            System.out.println("wasCar is true!");
+
+        // Exercise Start!
+
+        double extwenty = 20;
+        double exeighty = 80;
+
+        double exresult = extwenty + exeighty;
+        double initialresult = exresult;
+        initialresult =  exresult * 25;
+        System.out.println("Initial Result = " + initialresult);
+        initialresult = exresult % 40;
+        System.out.println("Remainder is = " + initialresult);
+        if(initialresult <= 20)
+            System.out.println("Total was over the limit!");
+
+
+
 
     }
 }
