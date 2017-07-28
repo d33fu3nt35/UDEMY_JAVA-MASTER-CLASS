049433c63c3ac790b9505c2864b70663d31d98e5
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-07-27 22:46:07 -0500

    Section 5 Lecture 30: Wen't over finishing Methods lecture and creating a hardcoded application that caluclates highscore & places you on highscore table based on your score wether it be 1 through 4. Also fixed bugs where the 'if else' statement needed greater then and EQUAL too instead of just greater than, ex. otherwise score of 1000 flat would be set to 4 instead of 1.

--- src/Expressions/Methods.java
+++ src/Expressions/Methods.java
@@ -9,7 +9,38 @@ public class Methods {
 
         calculateScore(true, 6000, 5, 100);
 
-        calculateScore(true, 10000, 8, 200);
+       int highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+
+        int highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Daniel", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Marc", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Emilio", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Evan", highScorePosition);
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition){
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if(playerScore >= 1000) {
+            return 1;
+        } else if(playerScore >= 500 && playerScore < 1000){
+            return 2;
+        } else if(playerScore >= 100 && playerScore < 500){
+            return 3;
+        } else{
+            return 4;
+        }
 
     }
 
@@ -18,7 +49,7 @@ public class Methods {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
+//            System.out.println("Your final score was " + finalScore);
             return finalScore;
         } else {
             return -1;
